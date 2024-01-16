03af01b347144b930e2d90c42b3002ad38bc6198
ArmanKhalili82 <hykhalili@yahoo.com>
2024-01-16 12:02:45 +0100

    GetAll

---
+++ MyProject/Controllers/InfoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Data;
+using MyProject.Models;
+
+namespace MyProject.Controllers
+{
+    public class InfoController : Controller
+    {
+        private static List<Info> PersonInfos = new List<Info>()
+        {
+            new Info()
+            {
+                Id = 1,
+                Name = "Ramin"
+            },
+
+            new Info()
+            {
+                Id = 2,
+                Name = "Amir"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            var db = new ApplicationDbContext();
+            var data = db.infos.Where()
+        }
+    }
+}
---
+++ MyProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProject.Models;
+
+namespace MyProject.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+        
+    }
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlServer(@"Server=.;DataBase=InfoTable;Encrypt=False;Integrated Security=True");
+
+    public DbSet<Info> infos { get; set; }
+}
